3fe19b83d637b37062ab07e890213671f190c6b4
Leszek Leszczynski <l.leszczynski@gmail.com>
2021-05-11 23:54:25 +0200

    sample webapp creation

--- main.tf
+++ main.tf
@@ -36,9 +36,46 @@ resource "azurerm_subnet" "learningsubnet" {
     address_prefixes       = ["10.0.2.0/24"]
 }
 
-module "azurevm" {
-  source = "./modules/azurevm"
+# module "azurevm" {
+#   source = "./modules/azurevm"
 
-  resource_group = azurerm_resource_group.learningrg.name
-  subnet = azurerm_subnet.learningsubnet.id
+#   resource_group = azurerm_resource_group.learningrg.name
+#   subnet = azurerm_subnet.learningsubnet.id
+# }
+
+# create app service plan
+resource "azurerm_app_service_plan" "webapp" {
+  name                = "webapp-appserviceplan"
+  location            = azurerm_resource_group.learningrg.location
+  resource_group_name = azurerm_resource_group.learningrg.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
 }
+
+# create webapp
+
+resource "azurerm_app_service" "webapp" {
+  name                = "webapp-app-service"
+  location            = azurerm_resource_group.learningrg.location
+  resource_group_name = azurerm_resource_group.learningrg.name
+  app_service_plan_id = azurerm_app_service_plan.webapp.id
+
+  source_control {
+    repo_url = "https://github.com/Azure-Samples/php-docs-hello-world"
+    branch = "master" #master is default, but just in case
+    manual_integration = true
+  }
+}
+
+output "app_service" {
+    value = azurerm_app_service.webapp.default_site_hostname
+}
+
+# Free Tier → F1
+# Shared Tier → D1
+# Basic Tier → B1, B2, B3 (Basic Small, Medium, Large)
+# Standard Tier → S1, S2, S3 (Small, Medium, Large)
+# PremiumV2 Tier → P1v2, P2v2, P3v2 (Small, Medium, Large)
